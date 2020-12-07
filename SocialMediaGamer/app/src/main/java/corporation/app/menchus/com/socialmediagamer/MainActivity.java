ca19e728b5171b3cb1183d71756e25fad7f35598
Carlos Menchu <carlosmenchu2@gmail.com>
2020-12-07 04:51:31 -0600

    Se agrego la pantalla de login y de registro

--- SocialMediaGamer/app/src/main/java/corporation/app/menchus/com/socialmediagamer/MainActivity.java
+++ SocialMediaGamer/app/src/main/java/corporation/app/menchus/com/socialmediagamer/MainActivity.java
@@ -2,13 +2,27 @@ package corporation.app.menchus.com.socialmediagamer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textViewRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewRegister = findViewById(R.id.textViewRegister);
+
+        textViewRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
